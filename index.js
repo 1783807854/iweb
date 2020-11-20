bc07a2f979cb3f89c2f3ab188f7a1ed4e949836c
1783807854 <73808289+1783807854@users.noreply.github.com>
2020-11-20 10:38:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let artPic = document.getElementsByClassName('artPic');
+let bg = document.getElementsByClassName('bg');
+let bgBlack = document.getElementsByClassName('bg-black');
+console.log(artPic)
+console.log(bg)
+// console.log(Array.isArray(artPic));
+// let artPicarr = Array.from(artPic);
+// console.log(artPic)
+// console.log(artPicarr)
+// for(let i = 0; i<=artPic.length; i++){
+//     console.log(1)
+//     artPic[i].onmousout = function(){
+//     console.log(2)
+//         for(let i = 0; i<=bg.length; i++){
+//             bg[i].classList.add('hover')
+//         }
+//     }
+// }
